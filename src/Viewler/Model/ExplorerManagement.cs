9d7e3c43740adb9ee26dc5b33af0c58c99f66d01
Zedlife <zombiecolony9@gmail.com>
2019-06-07 11:13:22 +0200

    in progress

---
+++ src/Viewler/Model/ExplorerManagement.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewler.Model {
+    class ExplorerManagement {
+        public string getFilePath() {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog()) {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            }
+        }
+    }
+}
